5c3ca4a1aca31eb8143bf0135ae684882845e9cf
lp177 <luperez@student.42.fr>
2014-11-09 04:57:18 +0100

    gitSync
    Don't forget to Up this Url with yours

---
+++ gitSync.sh
@@ -0,0 +1,10 @@
+#don't forget to Up this Url with yours
+myGit="https://github.com/myID/MygitRepo.git"
+previousSync="cp ~/.zshrc ~/ExtSync/;cp ~/.vimrc ~/ExtSync/"
+
+mkdir ~/Sync ~/ExtSync &> /dev/null
+cd ~/ExtSync
+git init &> /dev/null && git remote add origin $myGit &> /dev/null
+cd -
+alias Sync177="cd ~/ExtSync;diff -qr ~/Sync ~/ExtSync | grep 'Only.*ExtSync' | sed 's/Only.*ExtSync: //' | sed '/.git/d' | xargs git rm -rf --ignore-unmatch;rsync -vr ~/Sync/* --delete ~/ExtSync;find */ -name .git | sed 's/\/\//\//' | xargs git rm -vrf;$previousSync;git add *;git add -f .vimrc .zshrc;git commit -am 'Update `date`';git push origin master;cd -"
+alias gitSync="Sync177;Sync177"
