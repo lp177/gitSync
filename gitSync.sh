0770a24fb166177bb3345117cdcb6ed2c1a8ae4f
lp177 <luperez@student.42.fr>
2014-11-09 05:45:00 +0100

    Update gitSync.sh

--- gitSync.sh
+++ gitSync.sh
@@ -6,5 +6,5 @@ mkdir ~/Sync ~/ExtSync &> /dev/null
 cd ~/ExtSync
 git init &> /dev/null && git remote add origin $myGit &> /dev/null
 cd -
-alias Sync177="cd ~/ExtSync;diff -qr ~/Sync ~/ExtSync | grep 'Only.*ExtSync' | sed 's/Only.*ExtSync: //' | sed '/.git/d' | xargs git rm -rf --ignore-unmatch;rsync -vr ~/Sync/* --delete ~/ExtSync;find */ -name .git | sed 's/\/\//\//' | xargs git rm -vrf;$previousSync;git add *;git add -f .vimrc .zshrc;git commit -am 'Update `date`';git push origin master;cd -"
+alias Sync177="cd ~/ExtSync;diff -qr ~/Sync ~/ExtSync | grep 'Only.*ExtSync' | sed 's/Only.*ExtSync: //' | sed '/.git/d' | xargs git rm -rf --ignore-unmatch;rsync -r ~/Sync/* --delete ~/ExtSync;find */ -name .git | sed 's/\/\//\//' | xargs git rm -rf;find */ -name .git | sed 's/\/\//\//' | xargs rm -rf;$previousSync;git add ./*;git add -f .vimrc .zshrc;git commit -am 'Update `date`';git push origin master;cd -"
 alias gitSync="Sync177;Sync177"
