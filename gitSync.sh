6fc55e43ad0344c23958f763e50de1e09d9ce4d2
lp177 <luperez@student.42.fr>
2014-11-10 16:15:04 +0100

    Update gitSync.sh

--- gitSync.sh
+++ gitSync.sh
@@ -1,10 +1,16 @@
-#Don't forget to Up this Url with yours
-myGit="https://github.com/myID/MygitRepo.git"
+#Url of my repository
+myGit="https://github.com/lp177/42.git"
+#Command sh execute previous the push
 previousSync="cp ~/.zshrc ~/ExtSync/;cp ~/.vimrc ~/ExtSync/;git add -f .vimrc .zshrc"
 
-mkdir ~/Sync ~/ExtSync &> /dev/null
-cd ~/ExtSync
+#Dir wanted for Sync
+dirSync="Sync"
+#Dir target for temporary storage
+tmpSync="ExtSync"
+
+mkdir ~/$dirSync ~/$tmpSync &> /dev/null
+cd ~/$tmpSync
 git init &> /dev/null && git remote add origin $myGit &> /dev/null
 cd -
-alias Sync177="cd ~/ExtSync;diff -qr ~/Sync ~/ExtSync | grep 'Only.*ExtSync' | sed 's/Only.*ExtSync: //' | sed '/.git/d' | xargs git rm -rf --ignore-unmatch;rsync -r ~/Sync/* --delete ~/ExtSync;find */ -name .git | sed 's/\/\//\//' | xargs git rm -rf;find */ -name .git | sed 's/\/\//\//' | xargs rm -rf;$previousSync;git add ./*;git commit -am 'Update `date`';git push origin master;cd -"
+alias Sync177="cd ~/$tmpSync;diff -qr ~/$dirSync ~/$tmpSync | grep 'Only.*$tmpSync' | sed 's/Only.*$tmpSync: //' | sed '/.git/d' | xargs git rm -rf --ignore-unmatch;rsync -r ~/$dirSync/* --delete ~/$tmpSync;find */ -name .git | sed 's/\/\//\//' | xargs git rm -rf;find */ -name .git | sed 's/\/\//\//' | xargs rm -rf;$previousSync;git add ./*;git commit -am 'Update `date`';git push origin master;cd -"
 alias gitSync="Sync177;Sync177"
