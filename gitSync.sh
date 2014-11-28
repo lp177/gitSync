f74f03318ff37558e4d69faeccdea6c53bd733b7
lp177 <luperez@student.42.fr>
2014-11-28 20:49:46 +0100

    Update gitSync.sh

--- gitSync.sh
+++ gitSync.sh
@@ -14,6 +14,8 @@ git init &> /dev/null && git remote add origin $myGit &> /dev/null
 cd -
 alias Sync177="cd ~/$tmpSync;diff -qr ~/$dirSync ~/$tmpSync | grep 'Only.*$tmpSync' | sed 's/Only.*$tmpSync: //' | sed '/.git/d' | xargs git rm -rf --ignore-unmatch;rsync -r ~/$dirSync/* --delete ~/$tmpSync;find */ -name .git | sed 's/\/\//\//' | xargs git rm -rf;find */ -name .git | sed 's/\/\//\//' | xargs rm -rf;$previousSync;git add ./*;git commit -am 'Update `date`';git push origin master;cd -"
 alias gitSync="Sync177;Sync177"
+#short cmd
+#alias gs="gitSync"
 
 #Any problem with a ***lovely merge requiered and other infamous?
 #Go Raz repo and gitSync with this alias:
