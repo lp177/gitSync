d58250ae298b588c6f767b3a06d72c445dd52110
lp177 <luperez@student.42.fr>
2015-01-02 16:56:28 +0100

    Update gitSync.sh

--- gitSync.sh
+++ gitSync.sh
@@ -1,27 +1,17 @@
 #Url of my repository
 myGit="https://github.com/myID/Mygit.git"
-
-#Dir wanted for Sync (From ~)
-dirSync="Sync"
-
-#Dir target for temporary storage (From ~)
-tmpSync="ExtSync"
-
-#Command sh execute previous the push
-previousSync="cp ~/.zshrc ~/$tmpSync/;cp ~/.vimrc ~/$tmpSync/;git add -f .vimrc .zshrc"
-
 #Dir wanted for Sync
 dirSync="Sync"
 #Dir target for temporary storage
 tmpSync="ExtSync"
+#Command sh execute previous the push
+previousSync="\
+	cp -R ~/.vim ~/$tmpSync/vim;cp ~/.zshrc ~/$tmpSync/zshrc; \
+	cp ~/.vimrc ~/$tmpSync/vimrc;cp ~/cron_GitSync_177 ~/$tmpSync; \
+	git add -f ~/$tmpSync/vimrc ~/$tmpSync/zshrc ~/$tmpSync/vim \
+	~/$tmpSync/cron_GitSync_177"
 #interval in second into two auto sync (after launch cmd gitAutoSync)
 interval_auto_sync="600"
-#Command sh execute previous the push (gitSync)
-previousSync="cp -R ~/.vim ~/$tmpSync/vim;cp ~/.zshrc ~/$tmpSync/zshrc;cp ~/.vimrc ~/$tmpSync/vimrc;cp ~/cron_GitSync_177 ~/$tmpSync";git add -f ~/$tmpSync/vimrc ~/$tmpSync/zshrc ~/$tmpSync/vim ~/$tmpSync/cron_GitSync_177;
-#Command sh execute after the incoming clone (gitTake)
-afterTake="ls $dirSync;"
-#Expl of auoload config with afterTake:
-#"chmod 755 ~/.zshrc ~/.vimrc;cat ~/$dirSync/zshrc > ~/.zshrc;cat ~/$dirSync/vimrc > ~/.vimrc"
 
 echo "gitAutoSync" > ~/cron_GitSync_177
 mkdir ~/$dirSync ~/$tmpSync &> /dev/null
@@ -29,18 +19,40 @@ cd ~/$tmpSync
 git init &> /dev/null && git remote add origin $myGit &> /dev/null
 cd -
 
-#New stuff? and you haven't your local files for sync or just an duty obsolet copy ? Take your git with gitTake guy!
-alias gitTake="rm -rf ~/$tmpSync ~/$dirSync;git clone $myGit ~/$tmpSync;cp -R ~/$tmpSync ~/$dirSync;rm -rf ~/$dirSync/.git;$afterTake;"
-alias Sync177="cd ~/$tmpSync;diff -qr ~/$dirSync ~/$tmpSync | grep 'Only.*$tmpSync' | sed 's/Only.*$tmpSync: //' | sed '/.git/d' | xargs git rm -rf --ignore-unmatch;rsync -r ~/$dirSync/* --delete ~/$tmpSync;$previousSync;find */ -name .git | sed 's/\/\//\//' | xargs git rm -rf;find */ -name .git | sed 's/\/\//\//' | xargs rm -rf;git add ./*;git commit -am 'Update `date`';git push origin master;cd -"
-alias gitSync="Sync177;Sync177"
+alias gs="gitSync"
+alias gitTake="\
+	rm -rf ~/$tmpSync ~/$dirSync;git clone $myGit ~/$tmpSync;\
+	cp -R ~/$tmpSync ~/$dirSync;\
+	rm -rf ~/$dirSync/.git"
+alias gitSync="\
+	cd ~/$tmpSync;\
+	diff -qr ~/$dirSync ~/$tmpSync | grep 'Only.*$tmpSync' | \
+	sed 's/Only.*$tmpSync: //' | sed '/.git/d' | \
+	xargs git rm -rf --ignore-unmatch;\
+	rsync -r ~/$dirSync/* --delete ~/$tmpSync;\
+	$previousSync;\
+	find */ -name .git | sed 's/\/\//\//' | xargs git rm -rf;\
+	find */ -name .git | sed 's/\/\//\//' | xargs rm -rf;\
+	git add ./*;git commit -am 'Update `date`';git push origin master;cd -"
 
-#short cmd
-#alias gs="gitSync"
-
-#Any problem with a ***lovely merge requiered and other infamous thougs?
 #Rm all & push
-alias gitReset="cd ~/$tmpSync;git pull;git pull origin master;git pull;git commit -am \"reset\";git push origin master;git rm -rf *;rm -rf *;git push origin master;cd -;gitSync"
-#Git reset is the power to impose the respect of your repository at the scope of all, use this warm with abuse
+alias gitReset="\
+	cd ~/$tmpSync;\
+	git pull;\
+	git pull origin master;\
+	git pull;\
+	git commit -am \"reset\";\
+	git push origin master;\
+	git rm -rf *;rm -rf *;\
+	git push origin master;\
+	cd -;\
+	gitSync"
 
 #Launch auto gitSync all interval_auto_sync seconde(s) (not require cron)
-alias gitAutoSync="clear;echo \"Start at: \";date;echo \"\n\";gitSync;echo \"\n\nEnd at: \";date;echo -n \"Pending next interval...\";sleep $interval_auto_sync;source ~/cron_GitSync_177"
+alias gitAutoSync="\
+	echo \"Start at: \";date;echo \"\n\";\
+	gitSync;\
+	echo \"\n\nEnd at: \";date;echo -n \"Pending next interval...\";\
+	sleep $interval_auto_sync;\
+	clear;\
+	source ~/cron_GitSync_177"
