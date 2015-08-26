e0153ca4b1192a6cf9bdc74f667137b8c38ae309
luperez <luperez@student.42.fr>
2015-08-26 11:06:35 +0200

    installer in debug

--- install.sh
+++ install.sh
@@ -1,23 +1,21 @@
-read -p "pseudo:" -r
-echo
-
-$try_path="$HOME/.gitSync/gitSync.sh"
-if [ ! -f $try_path ]; then
-	$try_path="./gitSync.sh"
-	if [ ! -f $try_path ]; then
+PATH_TRY="$HOME/.gitSync/gitSync.sh"
+if [ ! -f $PATH_TRY ]; then
+	PATH_TRY="gitSync.sh"
+	if [ ! -f $PATH_TRY ]; then
 		read -p "Location of gitSync:" -r
 		echo
-		$try_path = $REPLY
+		PATH_TRY = $REPLY
 	fi
 fi
 
-if [ -f $try_path ]
+if [ -f $PATH_TRY ]
 then
 	read -p "Git pseudo:" -r
 	echo
-	$addr="myGit=\"https://github.com/\""
-	$addr=$addr$REPLY"/"
+	addr="myGit='https://github.com/"
+	addr=$addr$REPLY"/"
 	read -p "Git repository name for sync:"
 	echo
-	$addr=$REPLY".git"
-	sed -i "0s/.*/$addr/" $try_path
+	addr=$REPLY".git'"
+	sed -i "0s|.*|$addr|" $PATH_TRY
+fi
