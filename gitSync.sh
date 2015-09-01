5eb552418fdbe34c60fdd214b6d3b943a676fa17
lp177 <lp177@student.42.fr>
2015-09-01 10:52:43 +0200

    eject -i for local var

--- gitSync.sh
+++ gitSync.sh
@@ -24,13 +24,14 @@ preserveGuest="$dirSync/.preserveGuest"
 interval_auto_sync=60
 
 # Target Cfg
-favorites="$HOME/.zshrc
-$HOME/.vimrc
-$HOME/.vim
-$HOME/.atom
-$HOME/.gitSync
-$HOME/.*.sh
-"
+favorites=(
+	"$HOME/.zshrc"
+	"$HOME/.vimrc"
+	"$HOME/.vim"
+	"$HOME/.atom"
+	"$HOME/.gitSync"
+	"$HOME/.*.sh"
+)
 
 # Colors
 red="\033[0;31m"
@@ -40,40 +41,48 @@ nc="\033[0m"
 
 #Command sh execute previous the save on git with gitSync alias (for save various scattered files/folders)
 
+function	myCp
+{
+	if [ -d "$saveFolder/$target" ]
+	then
+		echo "Rsync $1"
+		rsync -a "$1/" --delete --exclude '.git' "$saveFolder/$target/"
+	elif [ -f "" ]; then
+		cat "$1" > "$saveFolder/$target"
+	else
+		rm -rf "$saveFolder/$target"
+		cp -R "$1" "$saveFolder/$target"
+	fi
+}
+
 function extractItem
 {
 	if [ -z "$1" ] ||  ( [ ! -f "$1" ] && [ ! -d "$1" ] ); then
+		echo "$red$1 not found.$nc"
 		return 0
 	fi
-
-	local -i target="$(basename $1)"
-	
+echo "Previous base:$1"
+	local target="$(basename $1)"
+echo "After base:$target"
 	if [ -z "$2" ]
 	then
-		local -i $saveFolder="$tmp"
+		local $saveFolder="$tmp"
 	else
-		local -i $saveFolder="$2"
+		local $saveFolder="$2"
 	fi
-	
+
 	if [ $archiver -eq 1 ] && ( [ -f "$saveFolder/$target" ] || [ -d "$saveFolder/$target" ] )
 	then
-		cp -R "$saveFolder/$target" "$saveFolder/_$target"
+		myCp "$saveFolder/$target" "$saveFolder/_$target"
 	fi
-	if [ -f "$1" ]
+
+	myCp "$1" > "$saveFolder/$target"
+
+	if [ -d "$1" ] || [ -f "$1" ]
 	then
-		cat "$1" > "$saveFolder/$target"
-	elif [ -d "$1" ]
-	then
-		if [ -d "$saveFolder/$target" ]
-		then
-			echo "Rsync $1"
-			rsync -a "$1/"/ --delete --exclude '.git' "$saveFolder/$target/"
-		else
-			echo "Cp $1"
-			cp -R "$1" "$saveFolder/$target"
-		fi
+		echo "$green$target extracted.$nc"
 	else
-		cp "$1" > "$saveFolder/$target"
+		echo "$red$target not extracted.$nc"
 	fi
 }
 function saveMyCfg
@@ -94,6 +103,7 @@ function extractGuest
 {
 	for conf in $favorites
 	do
+		echo "extractGuest take $conf"
 		extractItem "$conf" "$preserveGuest"
 	done
 }
@@ -114,14 +124,19 @@ function uninfect
 # Load your cfg on guest device and preserve guest cfg in $preserveGuest folder
 function infect
 {
-	echo 'Do you want infect ?\n'
-	read -p 'Get your cfg on guest and preserve guest cfg in $preserveGuest folder (y/n):' -n 1 -r
+	echo 'Do you want infect ?'
+	echo 'Get your cfg on guest and preserve guest cfg in $preserveGuest folder (y/n):'
+	REPLY=''
+	read
 	echo
 	if [[ "$REPLY" =~ ^[Yy]$ ]]
 	then
 		extractGuest
 		getCfg
 		source "$HOME/.zshrc"
+		echo "Done."
+	else
+		echo "Have a good day."
 	fi
 }
 
@@ -198,7 +213,8 @@ function createFolder
 	if [ -z "$1" ] || [ -d "$1" ]; then
 		return 0
 	fi
-	mkdir $1# &> /dev/null
+	mkdir $1
+# &> /dev/null
 	if [ -d $1 ]; then
 		echo "$green Create Dir $(basename $1) at $1$nc"
 	else
